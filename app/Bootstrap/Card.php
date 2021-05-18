51b0531e04bd58fea09a78c9e4e96ecfcecf5a60
ari <aristide.ff@gmail.com>
2021-05-18 21:40:26 +0200

    test card

--- app/Bootstrap/Card.php
+++ app/Bootstrap/Card.php
@@ -10,6 +10,41 @@ use \APP\Bootstrap\Carrousel;
 class Card extends Carrousel{
 
     public function __construct(){
-        echo 1;
+        $this->card();
+
+    }
+
+    public function card(){
+        $photo = "pictures/logo/PredFacker_logo.png";
+        echo '<div class="card-deck" style="max-width: 18rem;">
+                  <div class="card">
+                    <img class="card-img-top" src="'.$photo.'" alt="Card image cap">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                  </div>
+                  <div class="card">
+                    <img class="card-img-top" src="'.$photo.'" alt="Card image cap">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                  </div>
+                  <div class="card">
+                    <img class="card-img-top" src="'.$photo.'" alt="Card image cap">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                  </div>
+        </div>';
+    }
+
+    public function photo(){
+
     }
 }
