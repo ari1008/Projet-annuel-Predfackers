504f44941fae205637a0a924754e02bc793f5ff5
ari <aristide.ff@gmail.com>
2021-06-02 22:33:41 +0200

    problems methode Billsend.php mother class Filesend

--- app/Models/Billsend.php
+++ app/Models/Billsend.php
@@ -3,6 +3,7 @@
 
 namespace app\Models;
 
+use APP\Database;
 use APP\Table\Warehouse;
 use FPDF;
 use APP\Table\User;
@@ -12,17 +13,48 @@ use APP\Models\Filesend;
 class Billsend extends Filesend{
     protected $test;
 
-    public function __construct(FPDF $pdf, $output, $id_address, $id_user, ){
+    public function __construct(FPDF $pdf, $output, $id_user ){
         $output = $output . "Billing_Fredpacker.pdf";
         $this->output = $output;
-        $this->id_address = $id_address;
         $this->id_user = $id_user;
         $this->pdf = $pdf;
         $this->pdf->AddPage();
         $this->pdf->SetFont('Arial','',14);
         $this->header();
+        //$this->write();
+        $this->content();
+        $pdf->Output();
+    }
+
+    public function write($html){
+        $this->pdf->Write(5,$html);
+    }
+
+    public function header()
+    {
+        $file = ROOT_FOLDER .  "/public/pictures/logo/PredFacker_logo.png";
+        $this->pdf->Image($file,170,6,30);
+        $html = 'FREDPACKERS';
+        $this->write($html);
+    }
+
+    public function content(){
+        $user = $this->utility(new User());
+        //$this->pdf->SetTextColor(0,0,255);
+        //$this->pdf->SetFont('','U');
+        $content = gettext("Facture pour Mr ") . $user[0] . " " . $user[1]  .  gettext(" :");
+        $this->pdf->Cell(0);
+        $this->pdf->SetLeftMargin(40);
+        $this->pdf->Write(20,$content);
+        $this->table();
+
 
     }
+
+    public function board(){
+
+    }
+
 
 
 }
--- pages/client/succesProduct.php
+++ pages/client/succesProduct.php
@@ -1,7 +1,14 @@
 
 <div class="container">
     <?php
-
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+    use APP\Models\Billsend;
+    $content = ob_get_clean();
+    require_once ROOT_FOLDER . "/fpdf/fpdf.php";
+    $output = ROOT_FOLDER . "/public/pdf/";
+    $pdf = new FPDF();
+    $billing = new Billsend($pdf, $output, $_GET["id"]);
     ?>
 </div>
 <?php
